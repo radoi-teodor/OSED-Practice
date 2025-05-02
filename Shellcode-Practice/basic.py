142a94d973c3eec3616591562f8645b8783d211e
Skidrow <skidrow@skid.com>
2025-05-02 11:01:08 -0700

    Implemented search in shellcode x86

--- Shellcode-Practice/basic.py
+++ Shellcode-Practice/basic.py
@@ -1,17 +1,64 @@
 import ctypes, struct
 from keystone import Ks, KS_ARCH_X86, KS_MODE_32
 
+# functie care ne permite sa avem comentarii in codul assembly
+# ca baietii de la keystone engine nu au nevoie de comentarii, ei citesc assembly cum citeam eu Creanga in copilarie
+def strip_comments(asm_code):
+    lines = asm_code.splitlines()
+    clean_lines = []
+    for line in lines:
+        stripped = line.split(';', 1)[0].rstrip()
+        if stripped:
+            clean_lines.append(stripped)
+    return '\n'.join(clean_lines)
+
 CODE = """
-    xor eax, eax        ; facem EAX 0
-    mov eax, fs:[0x30]  ; preluam adresa PEB in memoria EAX
-    mov eax, [eax + 0x0C]  ; salvam structura PEB->Ldr
-    mov eax, [eax + 0x1C]  ; ne ducem pe primul entry din
-                           ; InInitializationOrderModuleList (FLINK - forward LINK)
-    mov eax, [eax]         ; ne ducem pe adresa modulului din pointer
-    mov eax, [eax + 0x08]  ; adresa din lista -0x10 (pentru a afla adresa 
-                           ; _LDR_DATA_TABLE_ENTRY) +0x18 (pantru proprietatea DllBase) 
+start:
+    xor eax, eax        ;
+    mov eax, fs:[0x30]  ;
+    mov eax, [eax + 0x0C]  ; 
+    mov esi, [eax + 0x1c]  ; salvam pointer catre Flink in ESI
+
+next_module:
+    ; toate offset-urile vor fi -0x10 pentru a merge in baza _LDR_DATA_TABLE_ENTRY
+    mov ebx, [esi + 0x08]    ; + 0x18 pentru a merge in DllBase si rezolvam si adresa pointerului
+
+    ; ATENTIE - proprietatea _UNICODE_STRING din _LDR_DATA_TABLE_ENTRY nu e pointer, e salvata ca structura in memorie
+    mov edi, esi     ; mutam temporar ESI in EDI pentru a aduna dupa, sa rezulte adresa _UNICODE_STRING
+    add edi, 0x1c    ; + 0x1c pentru a merge in BaseDllName (adresa catre _UNICODE_STRING)
+    mov edi, [edi + 0x04]    ; ne ducem in Buffer din _UNICODE_STRING
+
+check_char:
+    mov ax, [edi]            ; ne ducem in adresa pointer-ului Buffer din _UNICODE_STRING (primul caracter unicode)
+                             ; 
+    cmp al, 'K'              ; 
+    jne skip                 ; 
+    cmp ah, 0x00
+    jne skip
+
+    cmp word ptr [edi + 2], 0x0045 ; 0x0045 - 'E' unicode
+    jne skip
+    cmp word ptr [edi + 4], 0x0052 ; 0x0052 - 'R' unicode
+    jne skip
+    cmp word ptr [edi + 6], 0x004e ; 0x004e - 'N' unicode
+    jne skip
+
+    cmp word ptr [edi + 12], 0x0033 ; 0x0033 - '3' unicode (verificam daca al 7 lea caracter este '3' pentru a nu lua adresa KERNELBASE in loc de KERNEL32)
+    jne skip
+
+    jmp found
+
+skip:
+    mov esi, [esi]           ; trecem la urmatorul modul
+    jmp next_module
+
+found:
+    mov eax, ebx             ; salvam adresa de inceput a DLL-ului in EAX
     ret                    ;
 """
+
+
+CODE = strip_comments(CODE)
 
 ks = Ks(KS_ARCH_X86, KS_MODE_32)
 encoding, count = ks.asm(CODE)
@@ -29,15 +76,16 @@ ptr = ctypes.windll.kernel32.VirtualAlloc(ctypes.c_int(0),
                                           ctypes.c_int(0x40))
 
 # copiem shellcode-ul in memoria alocata
-buf = (ctypes.c_char * len(shellcode)).from_buffer(shellcode)
 ctypes.windll.kernel32.RtlMoveMemory(ctypes.c_int(ptr),
-                                     buf,
+                                     shellcode,
                                      ctypes.c_int(len(shellcode)))
 
 print("Shellcode located at address %s" % hex(ptr))
 
 # oprim executia script-ului pentru a putea conecta WinDBG la procesul python.exe
 input("...ENTER TO EXECUTE SHELLCODE...")
+
+ctypes.windll.kernel32.GetModuleHandleA(None)
 
 # creem thread-ul la adresa shellcode-ului
 ht = ctypes.windll.kernel32.CreateThread(ctypes.c_int(0),
